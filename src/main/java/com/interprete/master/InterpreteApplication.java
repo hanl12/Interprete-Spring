3efc21db30788e00da0124a84bd4bf2e768b45c0
hyrre <hyrre9056@gmail.com>
2020-05-18 20:57:53 -0500

    Probando con Spring

---
+++ src/main/java/com/interprete/master/InterpreteApplication.java
@@ -0,0 +1,15 @@
+package com.interprete.master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class InterpreteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InterpreteApplication.class, args);
+		
+	}
+
+}
